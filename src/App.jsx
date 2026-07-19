915594ce4205ec768567965684a231db0134b33e
mpdissanayake <165764981+mpdissanayake@users.noreply.github.com>
2026-07-19 11:35:51 +0530

    flexboxfixed_practice

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,26 @@ function App() {
   return (
     <>
       
-        <div >
+        <div className='w-[700px] h-[700px] border-[6px] flex justify-center items-center  relative' >
 
-        <TrendingProducts/>
+          <div className='w-[600px] h-[600px] bg-yellow-500 flex-col items-center justify-center' >
+
+            <div className='w-[70px] h-[70px] bg-red-700' >
+            </div>
+            <div className='w-[70px] h-[70px] bg-blue-600' >
+            </div>
+            <div className='w-[70px] h-[70px] bg-green-600 fixed bottom-[50px] right-[50px]' >
+            </div>
+            <div className='w-[70px] h-[70px] bg-orange-600 absolute top-[30px] right-[30px]' >
+            </div>
+            <div className='w-[70px] h-[70px] bg-white' >
+            </div>
+            <div className='w-[70px] h-[70px] bg-pink-400' >
+            </div>
+
+          </div>
+
+        
           
                   
           
